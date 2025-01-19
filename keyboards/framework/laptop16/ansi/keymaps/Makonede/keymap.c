f50608f27ea6ed087cac732619d7924e1dc5e724
Mako <61922615+Makonede@users.noreply.github.com>
2025-01-19 12:11:25 -0800

    Use uint8_t VideoCommand storage
    Signed-off-by: Mako <61922615+Makonede@useres.noreply.github.com>

--- keyboards/framework/laptop16/ansi/keymaps/Makonede/keymap.c
+++ keyboards/framework/laptop16/ansi/keymaps/Makonede/keymap.c
@@ -102,7 +102,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-typedef enum VideoCommand : bool {
+typedef enum VideoCommand : uint8_t {
   VIDEO_INIT,
   VIDEO_FRAME,
   VIDEO_END,
