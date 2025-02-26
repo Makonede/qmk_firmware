9163754e9053a395547f7408b05c0e437ef13ac1
Mako <61922615+Makonede@users.noreply.github.com>
2025-02-26 10:22:57 -0800

    Use EEPROM functions
    Signed-off-by: Mako <61922615+Makonede@users.noreply.github.com>

--- keyboards/framework/laptop16/ansi/keymaps/Makonede/keymap.c
+++ keyboards/framework/laptop16/ansi/keymaps/Makonede/keymap.c
@@ -134,7 +134,7 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     case VIDEO_INIT: {
       // Save current matrix mode and initialize video data
       videoMode = rgb_matrix_get_mode();
-      rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_blank);
+      rgb_matrix_mode(RGB_MATRIX_CUSTOM_blank);
       rgb_matrix_set_color_all(RGB_OFF);
       videoInit = report.data.init;
       videoFrameSize = videoInit.leds * (videoInit.rgb ? 3 : 1);
@@ -186,6 +186,6 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     case VIDEO_END:
       // Free frame buffer and reset original matrix mode
       free(videoFrame);
-      rgb_matrix_mode_noeeprom(videoMode);
+      rgb_matrix_mode(videoMode);
   }
 }
